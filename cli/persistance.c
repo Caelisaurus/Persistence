e9bc932f9c876896579af94e7d1054d7f78e9cb1
Ben Saltz <bsaltz@oberlin.edu>
2018-12-01 14:04:53 -0500

    Seperated cli and arduino version

---
+++ cli/persistance.c
@@ -0,0 +1,14 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  persistance.c
+ *
+ *    Description:  The initilization program for the Persistance Game
+ *
+ *        Version:  0.1 ALPHA
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+#include "persistance.h"
